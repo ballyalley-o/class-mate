7b43e70b18ac624bdf4332eebf1117bfebbb9a9f
bally <108769285+ballyalley-o@users.noreply.github.com>
2023-10-27 01:42:45 +1300

    initialized

---
+++ app.ts
@@ -0,0 +1,15 @@
+import express, { Request, Response } from 'express'
+
+const app = express()
+
+app.get('/', (req: Request, res: Response) => {
+  res.send('Hello, world!')
+})
+
+app.listen(
+  3000,
+
+  () => {
+    console.log('Server is listening on port 3000')
+  }
+)
---
+++ middleware/logger.ts
@@ -0,0 +1,7 @@
+import 'colors'
+
+const logger = (message: any) => {
+  console.log(message)
+}
+
+export default logger
