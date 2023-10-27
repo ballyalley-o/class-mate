6040f231721a7e454ea84ebb8c7c2c09bd2af876
bally <108769285+ballyalley-o@users.noreply.github.com>
2023-10-28 04:59:47 +1300

    App class

--- config/server.ts
+++ config/server.ts
@@ -1,1 +1,17 @@
-import express from 'express'
+import express, { Application, Request, Response } from 'express'
+
+class App {
+  private app: Application
+
+  constructor() {
+    this.app = express()
+    this.app.use(express.json())
+    this.registerRoutes()
+  }
+
+  private registerRoutes() {
+    mainRoute(this.app)
+  }
+
+  public start() {}
+}
