9191203238997620affac79653e934c178da77b7
bally <108769285+ballyalley-o@users.noreply.github.com>
2023-10-28 05:06:55 +1300

    router for cohort

---
+++ routes/cohort.ts
@@ -0,0 +1,12 @@
+import express from 'express'
+
+const router = express.Router()
+
+/**
+ * @path - baseUrl/api/v0.1/
+ */
+
+router.get('/')
+
+const cohortRouter = router
+export default cohortRouter
