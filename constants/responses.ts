fc15c6add62b7b5be7e443d7454113605d151406
bally <108769285+ballyalley-o@users.noreply.github.com>
2023-10-28 19:29:54 +1300

    server response and constants folder

---
+++ constants/responses.ts
@@ -0,0 +1,10 @@
+import { GLOBAL } from '@config'
+
+const RESPONSE = {
+  server: {
+    name: 'Class Automate',
+    status: 'Running',
+    API: 'class-server',
+    url: GLOBAL.baseUrl + GLOBAL.apiRoot,
+  },
+}
