e7e47936ce616e8e83a61667bee80032c0f094a0
bally <108769285+ballyalley-o@users.noreply.github.com>
2023-10-28 21:46:15 +1300

    prod constants

--- routing/param.ts
+++ routing/param.ts
@@ -1,6 +1,12 @@
+import path from 'path'
+
 const PARAM = {
+  home: '/',
   cohort: '/cohort',
   student: '/student',
+  // @production
+  buildLoc: 'client/dist',
+  buildView: path.resolve(__dirname, 'client', 'dist', 'index.html'),
 }
 
 export default PARAM
