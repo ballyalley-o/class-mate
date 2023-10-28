cacac143837ec5f54b18aa91118a319dcf919a1a
bally <108769285+ballyalley-o@users.noreply.github.com>
2023-10-28 19:29:25 +1300

    global params and export

---
+++ config/global.ts
@@ -0,0 +1,10 @@
+import dotenv from 'dotenv'
+dotenv.config()
+
+const GLOBAL = {
+  baseUrl: process.env.BASE_URL,
+  apiRoot: process.env.API_ROOT,
+  port: process.env.PORT,
+}
+
+export default GLOBAL
