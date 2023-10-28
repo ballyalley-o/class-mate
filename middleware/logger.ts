0d2c34c40877c62b39393756c1058ad8228cabe8
bally <108769285+ballyalley-o@users.noreply.github.com>
2023-10-28 15:13:20 +1300

    logger interface and middleware

--- middleware/logger.ts
+++ middleware/logger.ts
@@ -1,17 +1,45 @@
 import 'colors'
+import { Logger } from '@interfaces'
 
 declare module 'colors' {
   interface String {
     yellow: string
+    bgCyan: string
+    bgRed: string
+    red: string
   }
 }
 
-interface Logger {
-  (...message: any[]): void
-}
+const logger: Logger = {
+  // @type - custom
+  custom: (color: any, ...message: string[]) => console.log(...message[color]),
 
-const logger: Logger = (message) => {
-  console.log(message.join(' ').yellow)
+  // @type - log :Default
+  log: (...message: string[]) => console.log(...message.join(' ').yellow),
+
+  // @type - info
+  info: (...message: string[]) => console.log(...message.join(' ').bgCyan),
+
+  // @type - warn
+  warn: (...message: string[]) => console.warn(...message.join(' ').yellow),
+
+  // @type - error
+  error: (...message: string[]) =>
+    console.log(new Error(message.join(' ').bgRed)),
+
+  // @type - debug
+  debug: (...message: string[]) => console.debug(...message.join(' ').red),
+
+  // @preset - database
+  db: (db: any, isConnected: boolean) => {
+    console.log('DB HOST: '.bgGreen, db.connection.host.yellow)
+    console.log('DB NAME: '.bgGreen, db.connection.name.yellow)
+    if (isConnected) {
+      console.log('DB STATUS: '.bgGreen, 'CONNECTED'.green)
+    } else {
+      console.log('DB STATUS: '.bgRed, 'NO CONNECTION'.red)
+    }
+  },
 }
 
 export default logger
