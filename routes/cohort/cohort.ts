d0b96aaf4a3cdd52486863e001ca91b0052c1992
bally <108769285+ballyalley-o@users.noreply.github.com>
2023-10-28 19:30:31 +1300

    cohort routes

--- routes/cohort/cohort.ts
+++ routes/cohort/cohort.ts
@@ -3,9 +3,8 @@ import express from 'express'
 const router = express.Router()
 
 /**
- * @path - baseUrl/api/v0.1/
+ * @path - baseUrl/api/v0.1/cohort
  */
-
 router.get('/')
 
 const cohortRouter = router
--- routes/index.ts
+++ routes/index.ts
@@ -1,10 +1,12 @@
-import cohortRouter from '@routes/cohort'
+import cohortRouter from '@routes/cohort/cohort'
+import studentRouter from '@routes/cohort/students'
 import PARAM from '@routing/param'
 
 const cohortRoutes = cohortRouter
 
 const mainRoute = (app: any) => {
   app.use(PARAM.cohort, cohortRoutes)
+  app.use(PARAM.student)
 }
 
 export default mainRoute
