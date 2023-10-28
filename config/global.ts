cb0876150e7bd02ed089a73e45e59f12f317d888
bally <108769285+ballyalley-o@users.noreply.github.com>
2023-10-28 21:48:48 +1300

    constants max-age for jwt

--- config/global.ts
+++ config/global.ts
@@ -5,6 +5,7 @@ const GLOBAL = {
   baseUrl: process.env.BASE_URL,
   apiRoot: process.env.API_ROOT,
   port: process.env.PORT,
+  env: process.env.ENV,
 }
 
 export default GLOBAL
---
+++ constants/index.ts
@@ -0,0 +1,9 @@
+export { default as RESPONSE } from '@constants/responses'
+export {
+  expire,
+  oneHr,
+  twoHr,
+  sixHr,
+  oneDay,
+  thirtyDays,
+} from '@constants/max-age'
