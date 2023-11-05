9c9d7acba5418acff85ac529cba83e694063d600
bally <108769285+ballyalley-o@users.noreply.github.com>
2023-11-05 21:24:32 +1300

    __dirname import and RESPONSE refactor

--- routes/server.ts
+++ routes/server.ts
@@ -2,18 +2,17 @@ import path from 'path'
 import express, { Request, Response } from 'express'
 // @constants
 import { RESPONSE } from '@constants'
-import { GLOBAL } from '@config'
+import { GLOBAL, __dirname } from '@config'
 import { PATH, PARAM } from '@routing/index'
 
 const PROD_ENV = 'production'
 
 const serverRoute = (app: any) => {
   if (GLOBAL.env === PROD_ENV) {
-    const __dirname = path.resolve()
     app.use(express.static(path.join(__dirname, PARAM.buildLoc)))
     app.get('*', (req: Request, res: Response) => res.sendFile(PARAM.buildView))
   } else {
-    app.get(PATH.root, () => RESPONSE.server)
+    app.get(PATH.root, RESPONSE.server)
   }
 }
 
