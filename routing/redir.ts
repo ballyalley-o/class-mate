7e050fcc873a16753f89a3581486721919a01d03
bally <108769285+ballyalley-o@users.noreply.github.com>
2023-11-05 17:10:59 +1300

    redir func for param routing

--- routing/redir.ts
+++ routing/redir.ts
@@ -1,7 +1,15 @@
+import GLOBAL from '@config/global'
+
 interface Params {
-  (...params: string[]): void
+  (...params: any[]): void
 }
 
 const redir: Params = (...params) => {
   return params.join('')
 }
+
+const PATH = {
+  root: redir(GLOBAL.apiRoot, '/'),
+}
+
+export default PATH
