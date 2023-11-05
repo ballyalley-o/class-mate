f1be9c6c9ef6d472ac0850a430f4bec5261538e4
bally <108769285+ballyalley-o@users.noreply.github.com>
2023-11-06 00:15:36 +1300

    async handler middleware

---
+++ middleware/async-handler.ts
@@ -0,0 +1,11 @@
+import { Request, Response, NextFunction } from 'express'
+
+const asyncHandler = (
+  fn: (req: Request, res: Response, next: NextFunction) => Promise<void>
+) => {
+  return (req: Request, res: Response, next: NextFunction) => {
+    Promise.resolve(fn(req, res, next)).catch(next)
+  }
+}
+
+export default asyncHandler
