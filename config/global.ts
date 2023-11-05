6197e39abe5110e990959ba04ac38a0a19dcd78c
bally <108769285+ballyalley-o@users.noreply.github.com>
2023-11-06 03:05:10 +1300

    db and jwt globals

--- config/global.ts
+++ config/global.ts
@@ -6,6 +6,15 @@ const GLOBAL = {
   apiRoot: process.env.API_ROOT,
   port: process.env.PORT,
   env: process.env.ENV,
+  // jwt
+  jwt_secret: process.env.JWT_SECRET,
+  jwt_exp: process.env.JWT_EXP,
+  // @db
+  db_uri: process.env.DB_URI,
+  db_host: process.env.DB_HOST,
+  db_name: (db: any) => db.connection.name,
+  // @def
+  cookie: 'cookie',
 }
 
 export default GLOBAL
