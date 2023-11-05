027c76e1306dfd68d52da1446ddf2e46c6b0d840
bally <108769285+ballyalley-o@users.noreply.github.com>
2023-11-06 00:14:57 +1300

    export for interface models

---
+++ interfaces/models/index.ts
@@ -0,0 +1,4 @@
+export { default as IStudent } from '@interfaces/models/Student'
+export { default as IRecording } from '@interfaces/models/Recording'
+export { default as ITimetable } from '@interfaces/models/Timetable'
+export { default as ITrainer } from '@interfaces/models/Trainer'
