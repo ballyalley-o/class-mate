4d6a8ce5265bee420dcd8528cfa1f89e3fa89100
bally <108769285+ballyalley-o@users.noreply.github.com>
2023-11-06 00:50:16 +1300

    students controller

--- controller/students.ts
+++ controller/students.ts
@@ -1,6 +1,23 @@
 import { Student } from '@models'
+import { asyncHandler, logger } from '@middleware'
 
 // @desc Student /set token
 // @route GET /api/v0.1/students
 // @access Public
-const authStudent = () => {}
+const authStudent = asyncHandler(async (req, res) => {
+  //   const { email, userName, password } = req.body
+
+  //   const student = await Student.findOne({ email, userName })
+  //   const matchPassword = await student?.matchPassword(password)
+
+  try {
+    res.send('Hellow')
+  } catch (error: any) {
+    logger.error(error.message)
+    res.status(500).json('Error in Login')
+  }
+})
+
+const studentController = { authStudent }
+
+export default studentController
