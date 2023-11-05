e3cd786d7a789429205e58f73e13ac12bfb3dd75
bally <108769285+ballyalley-o@users.noreply.github.com>
2023-11-05 21:23:57 +1300

    cohort routes

--- routes/cohort/cohort-student.ts
+++ routes/cohort/cohort-student.ts
@@ -3,7 +3,7 @@ import express from 'express'
 const router = express.Router()
 
 /**
- * @path - baseUrl/api/v0.1/cohort
+ * @path - baseUrl/api/v0.1/cohort/:id/student
  */
 router.get('/', (req, res) => {})
 router.get('/:id', (req, res) => {})
@@ -11,5 +11,5 @@ router.post('/', (req, res) => {})
 router.put('/:id', (req, res) => {})
 router.delete('/:id', (req, res) => {})
 
-const cohortRoute = router
-export default cohortRoute
+const cohortStudentRoute = router
+export default cohortStudentRoute
