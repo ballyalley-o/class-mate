74828ddda5b21be478ab3bcdfbfd5a8eaa245f46
bally <108769285+ballyalley-o@users.noreply.github.com>
2023-11-06 00:15:12 +1300

    student interface

---
+++ interfaces/models/Student.ts
@@ -0,0 +1,11 @@
+interface IStudent {
+  firstName: string
+  lastName: string
+  email: string
+  password: string
+  location: string
+  userName: string
+  avatar: string
+}
+
+export default IStudent
