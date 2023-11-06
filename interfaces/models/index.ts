4e275bf4662e81b4f4642ae4efcea04e464d942b
bally <108769285+ballyalley-o@users.noreply.github.com>
2023-11-06 21:40:03 +1300

    Usser interface

--- interfaces/models/index.ts
+++ interfaces/models/index.ts
@@ -1,4 +1,4 @@
-export { default as IStudent } from '@interfaces/models/Student'
+export { default as IUser } from '@interfaces/models/User'
 export { default as IRecording } from '@interfaces/models/Recording'
 export { default as ITimetable } from '@interfaces/models/Timetable'
 export { default as ITrainer } from '@interfaces/models/Trainer'
