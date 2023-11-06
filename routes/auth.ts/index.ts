2736b6923bd193f24210163a8a9ae45a97c54c0e
bally <108769285+ballyalley-o@users.noreply.github.com>
2023-11-06 21:57:17 +1300

    renamed folder

--- routes/auth.ts/index.ts
+++
@@ -1,8 +0,0 @@
-import authRoute from '@routes/auth/auth'
-import { PATH } from '@routing'
-
-const linkAuthRoutes = (app: any) => {
-  app.use(PATH.auth, authRoute)
-}
-
-export default linkAuthRoutes
