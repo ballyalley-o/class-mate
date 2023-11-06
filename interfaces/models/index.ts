1aeecf7e2761b258dce3124bee3de6e2a1486c25
bally <108769285+ballyalley-o@users.noreply.github.com>
2023-11-06 21:41:33 +1300

    unified students and trainer as user

--- interfaces/models/index.ts
+++ interfaces/models/index.ts
@@ -1,4 +1,3 @@
 export { default as IUser } from '@interfaces/models/User'
 export { default as IRecording } from '@interfaces/models/Recording'
 export { default as ITimetable } from '@interfaces/models/Timetable'
-export { default as ITrainer } from '@interfaces/models/Trainer'
