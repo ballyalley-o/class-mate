60bae4e851adebd406c90587cc678601ccd8a314
bally <108769285+ballyalley-o@users.noreply.github.com>
2023-11-06 21:33:56 +1300

    added responses

--- constants/response.ts
+++ constants/response.ts
@@ -15,13 +15,15 @@ const RESPONSE = {
   },
   success: {
     200: 'OK: Successful Request',
+    201: (user: any) => `Success: ${user} is Created`,
   },
   error: {
     500: 'Internal Server Error: An unexpected server error occurred.',
     404: 'Not Found: The requested resource could not be found.',
     403: 'Forbidden: Access to the requested resource is forbidden',
     401: 'Unauthorized: Authentication is required or credentials are invalid',
-    400: 'Bad Request: The request is malformed or invalid',
+    400: (user: any) => `400: ${user} already exists`,
+    invalid: 'Bad Request: The request is malformed or invalid.',
   },
 }
 
