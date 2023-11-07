d4368d83f01aaac8b0c7af0db761db81cc4e9d24
bally <108769285+ballyalley-o@users.noreply.github.com>
2023-11-08 01:54:42 +1300

    ✨ feat(added get students and get student controllers):

--- controller/students.ts
+++ controller/students.ts
@@ -1,20 +1,28 @@
-import { User } from '@models'
+import mongoose from 'mongoose'
+import { User, Role } from '@models'
 import { asyncHandler, logger } from '@middleware'
 import { RESPONSE } from '@constants'
+import ObjectID from 'bson-objectid'
 
-// @desc Student /set token
-// @route GET /api/v0.1/students
+const TAG = 'Student'
+
+// @desc All Students
+// @route GET /api/v0.1/student
 // @access Public
-const getStudents = asyncHandler(async (req, res) => {
-  //   const { email, username, password } = req.body
-
-  //   const student = await User.findOne({ email, username })
-  //   const matchPassword = await student?.matchPassword(password)
-
+const getStudents = asyncHandler(async (req, res, next) => {
   try {
-    res.status(200).json({
-      message: RESPONSE.success[200],
-    })
+    const role = await Role.findOne({ name: TAG })
+    if (role) {
+      const students = await User.find({
+        role: role._id,
+      })
+
+      res.status(200).json({
+        message: RESPONSE.success[200],
+        total: students.length,
+        students: students,
+      })
+    }
   } catch (error: any) {
     logger.error(error.message)
     res.status(401)
@@ -22,5 +30,24 @@ const getStudents = asyncHandler(async (req, res) => {
   }
 })
 
-const studentController = { getStudents }
+// @desc Student
+// @route GET /api/v0.1/student
+// @access Public
+const getStudent = asyncHandler(async (req, res, next) => {
+  const student = await User.findById(req.params.id).select('-password')
+
+  try {
+    const role = await Role.findOne({ name: TAG })
+    res.status(200).json({
+      message: RESPONSE.success[200],
+      student,
+    })
+  } catch (error: any) {
+    logger.error(error.message)
+    res.status(404)
+    throw new Error(RESPONSE.error[404])
+  }
+})
+
+const studentController = { getStudents, getStudent }
 export default studentController
