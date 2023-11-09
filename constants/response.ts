eb647ddb973e2088c2802afaacd315c67e007e88
bally <108769285+ballyalley-o@users.noreply.github.com>
2023-11-09 23:27:31 +1300

    🐎 ci(added responses):

--- constants/response.ts
+++ constants/response.ts
@@ -20,12 +20,13 @@ const RESPONSE = {
     deleted: (user: any) => `Successful Request: ${user} was DELETED`,
   },
   error: {
-    500: 'Internal Server Error: An unexpected server error occurred.',
-    404: 'Not Found: The requested resource could not be found.',
+    500: 'Internal Server Error: An unexpected server error occurred',
+    404: 'Not Found: The requested resource could not be found',
     403: 'Forbidden: Access to the requested resource is forbidden',
     401: 'Unauthorized: Authentication is required or credentials are invalid',
     400: (user: any) => `400: ${user} already exists`,
-    invalid: 'Bad Request: The request is malformed or invalid.',
+    invalid: 'Bad Request: The request is malformed or invalid',
+    notFound: (user: any) => `${user} is not found`,
   },
 }
 
