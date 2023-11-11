562e073e000fb76c05f7419d69d1efda1d2d3a2c
bally <108769285+ballyalley-o@users.noreply.github.com>
2023-11-12 02:58:56 +1300

    🦄 refactor(added fields on the interface of Timetable):

--- interfaces/models/Timetable.ts
+++ interfaces/models/Timetable.ts
@@ -1,7 +1,10 @@
+import { Schema } from 'mongoose'
 interface ITimetable {
+  week: number
   date: string
   time: TimeRanges
   location: string
+  cohort: Schema.Types.ObjectId
 }
 
 export default ITimetable
