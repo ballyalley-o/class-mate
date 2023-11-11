92b592ed1036b4f517d85d6a7799dc9757d142a7
bally <108769285+ballyalley-o@users.noreply.github.com>
2023-11-12 03:27:28 +1300

    error handler add-ons

--- middleware/error-handler.ts
+++ middleware/error-handler.ts
@@ -4,16 +4,18 @@ import { GLOBAL } from '@config'
 
 class ExtError extends Error {
   kind: string
+  errors: any[]
 
-  constructor(message: string, kind: string) {
+  constructor(message: string, kind: string, errors: any[]) {
     super(message)
     this.kind = kind
+    this.errors = errors
     this.name = 'ExtError'
   }
 }
 
 /**
- *
+ * Error Handler for global
  * @param err
  * @param req
  * @param res
@@ -32,6 +34,12 @@ const errorHandler = (
   if (err.name === 'CastError' && err.kind === 'ObjectId') {
     statusCode = 404
     message = RESPONSE.error[404]
+  }
+
+  if (err.errors) {
+    const errors = Object.values(err.errors).map((err: any) => err.message)
+    statusCode = 400
+    message: errors
   }
 
   res.status(statusCode).json({
