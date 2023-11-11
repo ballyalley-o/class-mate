6cd635cd6181206acd332f744e1ed40efeaae6cf
bally <108769285+ballyalley-o@users.noreply.github.com>
2023-11-12 03:35:10 +1300

    ✨ feat(added fields for recording schema):

--- models/Recording.ts
+++ models/Recording.ts
@@ -1,11 +1,19 @@
 import mongoose, { Schema, model, connect } from 'mongoose'
 import { IRecording } from '@interfaces/models'
+import DefaultSchema from '@models/Default'
 
 const RecordingSchema = new Schema<IRecording>(
   {
     title: {
       type: String,
       required: true,
+    },
+    content: {
+      type: String,
+      required: true,
+    },
+    topics: {
+      type: [String],
     },
     date: {
       type: Schema.Types.ObjectId,
@@ -20,12 +28,26 @@ const RecordingSchema = new Schema<IRecording>(
       type: String,
       required: true,
     },
+    cohort: {
+      type: Schema.Types.ObjectId,
+    },
+    isRaw: {
+      type: Boolean,
+      default: false,
+    },
   },
   {
+    toJSON: { virtuals: true },
+    toObject: { virtuals: true },
     collection: 'Recording',
     timestamps: true,
   }
 )
+
+RecordingSchema.add(DefaultSchema)
+RecordingSchema.index({ title: 1 })
+RecordingSchema.index({ content: 1 })
+RecordingSchema.index({ topics: 1 })
 
 const Recording = mongoose.model('Recording', RecordingSchema)
 export default Recording
