6c61317b8c293b1849c2bd392ab095bd26b0d6d7
bally <108769285+ballyalley-o@users.noreply.github.com>
2023-11-12 03:35:59 +1300

    ✨ feat(added new fields on timetable schema):

--- models/Timetable.ts
+++ models/Timetable.ts
@@ -1,8 +1,13 @@
 import mongoose, { Schema, model, connect } from 'mongoose'
 import { ITimetable } from '@interfaces/models'
+import DefaultSchema from '@models/Default'
 
 const TimetableSchema = new Schema<ITimetable>(
   {
+    week: {
+      type: Number,
+      required: true,
+    },
     date: {
       type: String,
       required: true,
@@ -15,12 +20,19 @@ const TimetableSchema = new Schema<ITimetable>(
       type: String,
       required: true,
     },
+    cohort: {
+      type: Schema.Types.ObjectId,
+      required: true,
+    },
   },
   {
     collection: 'Timetable',
     timestamps: true,
   }
 )
+
+TimetableSchema.add(DefaultSchema)
+TimetableSchema.index({ time: 1 })
 
 const Timetable = mongoose.model('Timetable', TimetableSchema)
 export default Timetable
