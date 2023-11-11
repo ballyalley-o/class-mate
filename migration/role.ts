fee4fc65f23aba72a098a5fa64cafd1ee0888276
bally <108769285+ballyalley-o@users.noreply.github.com>
2023-11-12 03:30:56 +1300

    🦄 refactor(changes for inthe fields terface of role):

--- migration/role.ts
+++ migration/role.ts
@@ -1,19 +1,19 @@
 const roles = [
   {
     _id: '5c3b08ae6d663a3c4e8f3566',
-    name: 'Student',
+    type: 'Student',
     description: 'Student role',
     isLead: false,
   },
   {
     _id: '5c3b08bf6d663a3c4e8f3567',
-    name: 'Trainer',
+    type: 'Trainer',
     description: 'Trainer role',
     isLead: false,
   },
   {
     _id: '5c3b08cf6d663a3c4e8f3568',
-    name: 'Trainer',
+    type: 'Trainer',
     description: 'Lead Trainer role',
     isLead: true,
   },
