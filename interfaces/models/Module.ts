2dfbb84f45c18816804511b60a488eadc81e7cef
bally <108769285+ballyalley-o@users.noreply.github.com>
2023-11-12 02:56:50 +1300

    ✨ feat(module interface):

---
+++ interfaces/models/Module.ts
@@ -0,0 +1,13 @@
+interface IModule {
+  content: string
+  pages: number
+  file: string
+  link: string
+  title: string
+  snippets: string
+  exercises: number
+  agenda: string[]
+  slug: string
+}
+
+export default IModule
