f54edbab13f506a59247cf1f35514fc3c610821f
bally <108769285+ballyalley-o@users.noreply.github.com>
2023-11-11 15:17:10 +1300

    🐎 ci(validate and update cohort controller):

--- controller/cohort.ts
+++ controller/cohort.ts
@@ -1,6 +1,7 @@
 import { Cohort, User } from '@models'
 import { asyncHandler, logger } from '@middleware'
 import { RESPONSE } from '@constants'
+import ObjectID from 'bson-objectid'
 
 const TAG = 'Cohort'
 
@@ -27,12 +28,14 @@ const getCohorts = asyncHandler(async (req, res, next) => {
 const getCohort = asyncHandler(async (req, res, next) => {
   try {
     const cohort = await User.findById(req.params.id)
-
     if (cohort) {
       res.status(200).json({
         message: RESPONSE.success[200],
         cohort,
       })
+    } else {
+      res.status(404)
+      throw new Error(RESPONSE.error[404])
     }
   } catch (error: any) {
     logger.error(error.message)
@@ -43,7 +46,7 @@ const getCohort = asyncHandler(async (req, res, next) => {
 // @desc  Add a Cohort
 // @route POST /api/v0.1/cohort
 // @access Private
-const AddCohort = asyncHandler(async (req, res, next) => {
+const addCohort = asyncHandler(async (req, res, next) => {
   let { name, students, trainers } = req.body
 
   const cohortExists = await Cohort.findOne({ name })
@@ -53,25 +56,91 @@ const AddCohort = asyncHandler(async (req, res, next) => {
     throw new Error(RESPONSE.error[400](name))
   }
 
-  const cohortBody = {
+  const newCohort = new Cohort({
     name,
-    students,
-    trainers,
-  }
+    students: ObjectID(students) || [],
+    trainers: ObjectID(trainers) || [],
+  })
 
-  const cohort = await Cohort.create(cohortBody)
+  try {
+    await newCohort.validate()
+    const cohort = await newCohort.save()
 
-  if (cohort) {
     res.status(201).json({
       message: RESPONSE.success[201](name || TAG),
       id: cohort._id,
-      body: cohortBody,
+      body: cohort,
     })
-  } else {
-    res.status(400)
-    throw new Error(RESPONSE.error.invalid)
+  } catch (error: any) {
+    if (error.errors) {
+      const errors = Object.values(error.errors).map((err: any) => err.message)
+      res.status(400).json({ error: errors })
+    } else {
+      res.status(400).json({ error })
+    }
   }
 })
 
-const cohortController = { getCohorts, getCohort, AddCohort }
+// @desc  Update a Cohort
+// @route PUT /api/v0.1/cohort/:id
+// @access Private
+const updateCohort = asyncHandler(async (req, res, next) => {
+  const { students, trainers, name } = req.body
+  const cohort = await Cohort.findById(req.params.id)
+
+  if (cohort) {
+    cohort.name = req.body.name || cohort.name
+
+    const studentExists = cohort.students.includes(students)
+    if (studentExists) {
+      res.status(400)
+      throw new Error(RESPONSE.error[400]('Student'))
+    }
+    cohort.students = [...cohort.students, req.body.students] || cohort.students
+
+    const trainerExists = cohort.trainers.includes(trainers)
+    if (trainerExists) {
+      res.status(400)
+      throw new Error(RESPONSE.error[400]('Trainer'))
+    }
+    cohort.trainers = [...cohort.trainers, req.body.trainers] || cohort.trainers
+
+    const updatedCohort = await cohort.save()
+
+    res.status(200).json({
+      message: RESPONSE.success.updated(updatedCohort.name),
+      id: updatedCohort._id,
+      updated: req.body,
+    })
+  } else {
+    res.status(404)
+    throw new Error(RESPONSE.error[404])
+  }
+})
+
+// @desc  Delete a Cohort
+// @route DELETE /api/v0.1/cohort/:id
+// @access Private
+const deleteCohort = asyncHandler(async (req, res, next) => {
+  const cohort = await Cohort.findById(req.params.id)
+
+  if (cohort) {
+    await cohort.deleteOne({ _id: cohort._id })
+    res.status(200).json({
+      message: RESPONSE.success.deleted(cohort.name),
+      cohort: {},
+    })
+  } else {
+    res.status(404)
+    throw new Error(RESPONSE.error[404])
+  }
+})
+
+const cohortController = {
+  getCohorts,
+  getCohort,
+  addCohort,
+  updateCohort,
+  deleteCohort,
+}
 export default cohortController
