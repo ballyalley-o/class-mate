104f7c48d7fb83c991a48aae64661e69cadb9e44
bally <108769285+ballyalley-o@users.noreply.github.com>
2023-11-11 15:19:39 +1300

    role validate export

---
+++ middleware/role-validate.ts
@@ -0,0 +1,22 @@
+import mongoose, { Types, Schema } from 'mongoose'
+import { Role } from '@models'
+import { IUser } from '@interfaces/models'
+
+type UserId = Types.ObjectId | IUser
+
+const TAG = 'User'
+
+/**
+ * middleware to secure the schema fields for only their respective roles
+ * @param role - roles to validate in the schema
+ * @returns bool
+ */
+
+async function roleValidate(userId: UserId, role: string): Promise<boolean> {
+  const user = await mongoose.model(TAG).findById(userId)
+  const roleType = await Role.findById(user.role)
+
+  return user && roleType?.name === role
+}
+
+export default roleValidate
