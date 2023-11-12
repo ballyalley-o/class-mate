e0de8fd12148bbc2e25bb9eca1dbc8dacba11b1b
bally <108769285+ballyalley-o@users.noreply.github.com>
2023-11-12 13:11:02 +1300

    🎉 init(Base schema add ons a):

--- models/Base.ts
+++ models/Base.ts
@@ -1,1 +1,11 @@
 import mongoose, { Schema } from 'mongoose'
+
+// TODO: add fields for role based
+interface ITrainer {
+  isChecked: boolean
+}
+
+interface IStudent {
+  submissions: string[]
+  labs: string[]
+}
