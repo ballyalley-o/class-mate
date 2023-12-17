a326b9cdc689a1e034d9c0b3a34a3d6b2dcaf097
bally <108769285+ballyalley-o@users.noreply.github.com>
2023-12-18 03:31:18 +1300

    module mock data for migration

---
+++ migration/module.ts
@@ -0,0 +1,38 @@
+const modules = [
+  {
+    title: 'Module 1',
+    content: 'Basics of Software Engineering',
+    pages: 89,
+    file: '',
+    link: 'http://example.com',
+    snippets:
+      'https://gist.github.com/ballyalley-o/6ef03c0e902d478ad319d9f51bed3065',
+
+    exercises: 4,
+    laboratory: [
+      {
+        module1: [
+          {
+            exercise1: {
+              repo: 'http://classroom.com/m1-e1',
+              status: 'Completed',
+            },
+            exercise2: {
+              repo: 'http://classroom.com/m1-e2',
+              status: 'Completed',
+            },
+            exercise3: {
+              repo: 'http://classroom.com/m1-e3',
+              status: 'Submitted',
+            },
+            exercise: {
+              repo: 'http://classroom.com/m1-e4',
+              status: 'Not Submitted',
+            },
+          },
+        ],
+      },
+    ],
+    agenda: ['HTML', 'JS', 'CSS'],
+  },
+]
