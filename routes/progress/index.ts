81a50ae9792f0fa1eb12f78d1f98278ebec04cdc
bally <108769285+ballyalley-o@users.noreply.github.com>
2024-01-08 22:51:16 +1300

    🎉 init(progress route):

---
+++ routes/progress/index.ts
@@ -0,0 +1,14 @@
+import progressRoute from '@routes/progress/progress'
+import statusRoute from '@routes/progress/progress-status'
+import { PATH } from '@routing'
+
+/**
+ * @path domain/api/v0.1/cohort
+ */
+// const base = `${PARAM.cohort.def}`
+const linkProgressRoutes = (app: any) => {
+  app.use(PATH.progress, progressRoute)
+  app.use(PATH.progress, statusRoute)
+}
+
+export default linkProgressRoutes
