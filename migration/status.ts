4f4b1fe27dbc2076a8226de6d216ec412b99644e
bally <108769285+ballyalley-o@users.noreply.github.com>
2024-01-08 22:43:04 +1300

    premade data for statuses

---
+++ migration/status.ts
@@ -0,0 +1,49 @@
+const roles = [
+  {
+    _id: '',
+    type: 'submitted',
+    description: 'Student role',
+  },
+  {
+    _id: '',
+    type: 'unsubmitted',
+    description: 'Trainer role',
+  },
+  {
+    _id: '',
+    type: 'accepted',
+    description: 'Lead Trainer role',
+  },
+  {
+    _id: '',
+    type: 'unaccepted',
+    description: 'Lead Trainer role',
+  },
+  {
+    _id: '',
+    type: 'failed automated test',
+    description: 'Lead Trainer role',
+  },
+  {
+    _id: '',
+    type: 'passed automated test',
+    description: 'Lead Trainer role',
+  },
+  {
+    _id: '',
+    type: 'incomplete',
+    description: 'Lead Trainer role',
+  },
+  {
+    _id: '',
+    type: 'completed',
+    description: 'Lead Trainer role',
+  },
+  {
+    _id: '',
+    type: 'waiting for student',
+    description: 'Lead Trainer role',
+  },
+]
+
+export default roles
