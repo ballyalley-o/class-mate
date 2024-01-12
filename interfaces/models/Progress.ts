d8317de63362fc53181be9bc049c18417623f062
bally <108769285+ballyalley-o@users.noreply.github.com>
2024-01-12 23:48:10 +1300

    🦄 refactor(process for students):

--- interfaces/models/Progress.ts
+++ interfaces/models/Progress.ts
@@ -1,6 +1,9 @@
 import { Schema } from 'mongoose'
 
 interface IProgress {
+  moduleExercises: Schema.Types.ObjectId[]
+  miniProjects: Schema.Types.ObjectId[]
+  capstone: Schema.Types.ObjectId
   exercise: Schema.Types.ObjectId
   status: Schema.Types.ObjectId
 }
